77704f1378c238e1aac39bfd4d4c451c13cdd870
Jack Skerrett <jackskerrett@gmail.com>
2023-05-10 17:56:46 +0100

    playing with colours

--- components/sidebar/sidebar.jsx
+++ components/sidebar/sidebar.jsx
@@ -1,19 +1,19 @@
-import "./sidebar.css"
+import "./sidebar.css";
 import { Link } from "react-router-dom";
 
-
-
 export default function Sidebar(props) {
-
-    return (
-        <div id="sidebar">
-            <h1>Featurephile</h1>
-            <ul id="featureSelector">
-                {props.features.map(feature => {
-                    return <li key={feature.id}><Link to={`/features/${feature.id}`}>{feature.name}</Link></li>
-                })}
-                
-            </ul>
-        </div>
-    )
+  return (
+    <div id="sidebar">
+      <h1>Featurephile</h1>
+      <ul id="feature-selector">
+        {props.features.map((feature) => {
+          return (
+            <li key={feature.id}>
+              <Link id="feature-link" to={`/features/${feature.id}`}>{feature.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
