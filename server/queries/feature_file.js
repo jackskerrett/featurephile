75acaee65bcaa7bb9baf79835d5708db32e2e184
Jack Skerrett <jackskerrett@gmail.com>
2023-07-02 15:44:43 +0100

    trying to get it so that a nice object is returned to the front end

--- server/queries/feature_file.js
+++ server/queries/feature_file.js
@@ -2,7 +2,17 @@ const pool = require("./config");
 
 async function get_feature_file(id) {
   const client = await pool.connect();
-  const query = `SELECT f.title AS feature_title, f.value_statement, r.title AS rule_title, r.id AS rule_id FROM features AS f INNER JOIN rules AS r ON f.id = r.feature_id WHERE feature_id=${id}`;
+  const query = `
+  SELECT f.title AS feature_title,
+  f.value_statement,
+  json_agg(r.title) AS rule_title,
+  json_agg(r.id) AS rule_id,
+  json_agg(json_build_object('scenario_title', sc.title, "scenario_id", sc.id)) as SCENARIOS
+  FROM features AS f
+  INNER JOIN rules AS r ON f.id = r.feature_id
+  INNER JOIN scenarios AS sc ON r.id = sc.rule_id
+  WHERE feature_id=${id}
+  GROUP BY f.title, f.value_statement`;
   const result = await client.query(query);
   const feature = result.rows;
   client.release();
