a78734ce5967828cdb1b3c24c06f6c8170e28de9
Jack Skerrett <jackskerrett@gmail.com>
2023-07-08 21:13:39 +0100

    things were broken so I reverted them

--- server/queries/feature_file.js
+++ server/queries/feature_file.js
@@ -2,17 +2,19 @@ const pool = require("./config");
 
 async function get_feature_file(id) {
   const client = await pool.connect();
+  console.log("here");
   const query = `
   SELECT f.title AS feature_title,
   f.value_statement,
   json_agg(r.title) AS rule_title,
   json_agg(r.id) AS rule_id,
-  json_agg(json_build_object('scenario_title', sc.title, "scenario_id", sc.id)) as SCENARIOS
+  sc.title AS scenario_title,
+  sc.id AS scenario_id
   FROM features AS f
   INNER JOIN rules AS r ON f.id = r.feature_id
   INNER JOIN scenarios AS sc ON r.id = sc.rule_id
   WHERE feature_id=${id}
-  GROUP BY f.title, f.value_statement`;
+  GROUP BY f.title, f.value_statement, sc.title, sc.id`;
   const result = await client.query(query);
   const feature = result.rows;
   client.release();
